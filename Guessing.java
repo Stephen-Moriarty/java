013f188d25b3017a3a2c8851981db2b9bf5b97a2
Stephen Moriarty <81331909+Stephen-Moriarty@users.noreply.github.com>
2021-10-08 18:19:38 -0400

    Add files via upload

--- Guessing.java
+++ Guessing.java
@@ -6,23 +6,29 @@ public class Guessing {
         // Making the random and scanner
         Random r = new Random();
         Scanner k = new Scanner(System.in);
-        //end
-        // System.out.println("Yeet");
         // Variables
-        int lives = 4;
+        int lives = 5;
         int counter = 0;
-        int lowRange = 0;
-        int highRange = 1000;
-        int randomNum = r.nextInt(2)+0;
+        //int lowRange = 0;
+        //int highRange = 100;
+        int randomNum = r.nextInt(100);
         boolean on = true;
         int userGuess = 0;
-        
-
-        System.out.println("Here are the rules: So you have 4 lives everytime you guess wrong");
+        //Start of outputs and inputs
+        System.out.println("Here are the rules: So you have 4 lives\nEverytime you guess wrong you get a life taken away\nYou have to guess a number between 1 and 100");
         System.out.println(randomNum);
+
         do {
             System.out.println("Guess a number: ");
             userGuess = k.nextInt();
+            if (userGuess == randomNum){
+                System.out.println("Yay! You got it correct.");
+                
+            }else{
+                System.out.println("Sorry that is wrong please try again.");
+                counter++;
+                lives--;
+            }
             counter++;
         } while (counter == 0);
         System.out.println("Post loop print statment");
