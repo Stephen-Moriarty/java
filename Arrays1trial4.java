4539e63fe7cd60a61766a161ae9aff616be00024
Stephen Moriarty <81331909+Stephen-Moriarty@users.noreply.github.com>
2021-11-23 22:23:23 -0500

    Still broke
    Getting like REAL close I think.

---
+++ Arrays1trial4.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Arrays1trial4 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        // Shifting alphabet
+        char[] alph = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        System.out.println("The Alphabet: " + Arrays.toString(alph));
+        System.out.print("How much do you want to shift the alphabet by?\n> ");
+        int userShiftVal = keyboard.nextInt();
+        // Create the new shifted array
+        char[] shiftedAlph = new char[alph.length];
+        int offSet = 0;
+        for(int i=0; i<alph.length; i++) {
+            offSet = i + userShiftVal;
+            shiftedAlph[i] = alph[offSet % alph.length];
+        }
+        // Print out finished shifted alphabet
+        System.out.println("Shifted alphabet: " + Arrays.toString(shiftedAlph));
+        // End of shifting alphabet
+
+        //spacer for readability and sectioning
+
+        // Shifting/encoding userWord section       
+        System.out.print("Give me a word to encode\n> ");
+        String userWord = keyboard.next();
+        keyboard.close();
+        char[] searcher = new char[userWord.length()];
+        for (int i=0; i<userWord.length(); i++) {
+            for (int j=0; j<alph.length; j++){
+                searcher[i] = userWord.charAt(i);
+                if (userWord.charAt(i) == shiftedAlph[i]) {
+                    System.out.println(shiftedAlph[j]);
+            } 
+          }
+        }
+
+    }
+}
