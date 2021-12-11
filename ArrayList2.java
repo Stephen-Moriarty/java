8bf23b5687f0656fd6413c680d624733bd4ace5c
Stephen Moriarty <81331909+Stephen-Moriarty@users.noreply.github.com>
2021-12-10 22:36:41 -0500

    The assignment
    The assignment that I am working on horribly.

---
+++ ArrayList2.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class ArrayList2 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        ArrayList<String> partyList = new ArrayList<String>();
+        System.out.println("You know who NOT to invite.");
+        System.out.println("Who you want in the party list?");
+        // Who we not invite
+        String blackSheep = "Adam";
+
+        // Get invites
+        do {
+            System.out.print("> ");
+            partyList.add(keyboard.next());
+            // Remove blacksheep
+            
+            /*
+            if (keyboard.next() == "Adam") {
+                partyList.remove(blackSheep);
+            }
+            */
+        } while(!partyList.get(partyList.size()-1).equalsIgnoreCase(blackSheep));
+        keyboard.close();
+        // Sort the list
+
+        Collections.sort(partyList);
+        //String name = "yeet";
+        //name = keyboard.nextLine();
+        // Check for black sheep
+        // Grrrr.berate
+        // Print list in abc order
+        /*
+        partyList.add("");
+        partyList.add("");
+        partyList.add(name);
+        */
+        // Print the stuff
+        System.out.println("No '" + blackSheep + "' Here is who you invited before you messed up:");
+        System.out.println(partyList);
+
+        // The blacklist
+        ArrayList<String> blackList = new ArrayList<String>();
+        blackList.add("Temp");
+        blackList.add("Mada");
+        blackList.add("Derik");
+        blackList.add("Jesus");
+        blackList.add("Ashley");
+        blackList.add("Omar");
+        blackList.add("Adam");
+        partyList.removeAll(blackList);
+        System.out.println("Here is what is after filtering your list:\n" + partyList);
+    }
+}
